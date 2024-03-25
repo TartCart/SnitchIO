2a0ebd780c8c87e59b23437444ffcc7fd94229ef
Michael Carlson <mikclson@gmail.com>
2024-03-25 13:36:18 -0400

    added front end changes to config page to allow for exclusions

--- ElectronUI/scripts/configure-renderer.js
+++ ElectronUI/scripts/configure-renderer.js
@@ -1,6 +1,6 @@
-document.getElementById('submit-btn').addEventListener('click', function() {
+// email submit instructions
+document.getElementById('email-submit-btn').addEventListener('click', function() {
 
-    // TODO: clean the array, remove spaces and confirm its an email address etc 
     const emailList = document.getElementById('email-list').value;
     const emailArray = emailList.split(","); 
     const emailObject = {
@@ -22,13 +22,48 @@ document.getElementById('submit-btn').addEventListener('click', function() {
             // fade out the received confirmation
         setTimeout(function() {
             document.getElementById('submit-status').textContent = '';
-        }, 2000)
+        }, 5000)
 
         try {
             window.electronAPI.sendData(emailObject);
             console.log('update email array sent successfully');
           } catch (error) {
             console.error('Error sending update email array:', error);
+          }
+    }
+});
+
+// exclusion submit isntructions
+document.getElementById('exclusions-submit-btn').addEventListener('click', function() {
+ 
+    const exclusionList = document.getElementById('exclusions-list').value;
+    const exclusionArray = exclusionList.split(","); 
+    const exclusionObject = {
+        name: 'exclusion-array',
+        content: exclusionArray,
+        update: true
+    }
+    if (exclusionList === '')
+    {
+        document.getElementById('exclusion-submit-status').textContent = 'No exclusions entered..';
+        
+        // fade out the error notification
+        setTimeout(function() {
+            document.getElementById('exclusion-submit-status').textContent = '';
+        }, 2000)
+        return;
+    } else {
+        document.getElementById('exclusion-submit-status').textContent = 'Recieved';
+            // fade out the received confirmation
+        setTimeout(function() {
+            document.getElementById('exclusion-submit-status').textContent = '';
+        }, 5000)
+
+        try {
+            window.electronAPI.sendData(exclusionObject);
+            console.log('exclusion array sent successfully');
+          } catch (error) {
+            console.error('Error sending exclusion array:', error);
           }
     }
 });
